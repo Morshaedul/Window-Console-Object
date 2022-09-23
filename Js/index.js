d8883786fd7b375e59762452ab36b41bcd6e2110
Morshaedul <morshaedulanirbanit@gmail.com>
2022-09-23 22:59:46 +0600

    first commit

---
+++ Js/index.js
@@ -0,0 +1,14 @@
+window.console.error("You made a mistake");
+
+
+
+
+
+
+
+
+// var date =new Date();
+// var currentDate=date.getFullYear();
+// var Date=date.getDay();
+// document.write(currentDate);
+// document.write(date);
